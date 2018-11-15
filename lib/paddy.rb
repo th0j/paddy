7518bb65c6e3600c03afd30d59ad86fc48988dea
kensupermen <votantho93@gmail.com>
2018-11-15 21:54:25 +0700

    add rack

--- lib/paddy.rb
+++ lib/paddy.rb
@@ -1,5 +1,9 @@
-require "paddy/version"
+require 'paddy/version'
 
 module Paddy
-  # Your code goes here...
+  class Application
+    def call(env)
+      [200, {'Content-Type' => 'text/html'}, ['Hello from Ruby on Rulers!']]
+    end
+  end
 end
