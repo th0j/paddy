5983868378b6c5f54e3bb35fe39f2a000c07923f
kensupermen <votantho93@gmail.com>
2018-11-24 15:55:39 +0700

    create for FileModel

--- examples/best_quotes/app/controllers/quotes_controller.rb
+++ examples/best_quotes/app/controllers/quotes_controller.rb
@@ -1,4 +1,9 @@
 class QuotesController < Paddy::Controller
+  def index
+    quotes = FileModel.all
+    render index, quotes: quotes
+  end
+
   def a_quote
     render :a_quote, noun: :winking
   end
@@ -7,6 +12,17 @@ class QuotesController < Paddy::Controller
     # quote_a = Paddy::Model::FileModel.find(1)
     quote_a = FileModel.find(1)
     render :quote, obj: quote_a
+  end
+
+  def new_quote
+    attrs = {
+      submitter: 'web_user',
+      quote: 'A picture is worth a thousand pixels',
+      attribution: 'Me'
+    }
+
+    m = FileModel.create attrs
+    render :quote, obj: m
   end
 end
 
